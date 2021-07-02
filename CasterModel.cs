38824356693c050917d61574f3a00bd651d37d21
KevDev23 <30936958+KevDev23@users.noreply.github.com>
2021-07-01 22:14:38 -0500

    Character Creation Window added

--- CasterModel.cs
+++ CasterModel.cs
@@ -10,17 +10,116 @@ namespace magic
     /// Character level, for display purposes and determining maxHP
     /// </summary>
         public int level { get; set; }
+
         /// <summary>
         /// The amount of magic points a character currently has
         /// </summary>
         public int mp { get; set; } 
+
         /// <summary>
         /// The magic point maximum determined by level
         /// </summary>
         public int maxMP { get; set; }
+
         /// <summary>
         /// The maximum mp a character can have determined by level
         /// </summary>
         public string characterName  { get; set; }
+        
+        /// <summary>
+        /// sets the maxMP based on level.
+        /// </summary>
+        public void determineMaxMP()
+        {
+            switch(level)
+            {
+                case 1:
+                    maxMP = 4;
+                    break;
+
+                case 2:
+                    maxMP = 6;
+                    break;
+
+                case 3:
+                    maxMP = 14;
+                    break;
+
+                case 4:
+                    maxMP = 17;
+                    break;
+
+                case 5:
+                    maxMP = 27;
+                    break;
+
+                case 6:
+                    maxMP = 32;
+                    break;
+
+                case 7:
+                    maxMP = 38;
+                    break;
+
+                case 8:
+                    maxMP = 44;
+                    break;
+
+                case 9:
+                    maxMP = 57;
+                    break;
+
+                case 10:
+                    maxMP = 64;
+                    break;
+
+                case 11:
+                    maxMP = 73;
+                    break;
+
+                case 12:
+                    maxMP = 73;
+                    break;
+
+                case 13:
+                    maxMP = 83;
+                    break;
+
+                case 14:
+                    maxMP = 83;
+                    break;
+
+                case 15:
+                    maxMP = 94;
+                    break;
+
+                case 16:
+                    maxMP = 94;
+                    break;
+
+                case 17:
+                    maxMP = 107;
+                    break;
+
+                case 18:
+                    maxMP = 114;
+                    break;
+
+                case 19:
+                    maxMP = 123;
+                    break;
+
+                case 20:
+                    maxMP = 133;
+                    break;
+
+                default:
+                    Console.WriteLine("Error: In determineMaxMP, level is: ", level);
+                    break;
+
+
+            }
+            
+        }
     }
 }
---
+++ characterCreation.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace magic
+{
+    /// <summary>
+    /// Interaction logic for characterCreation.xaml
+    /// </summary>
+    public partial class characterCreation : Window
+    {
+        public characterCreation()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCharacter_accept(object sender, RoutedEventArgs e)
+        {
+            
+            CasterModel newCharacter = new CasterModel();
+
+            //initialize the character's name, level, maxMP, and mp
+            if (!(name.Text.Equals(String.Empty)) && checkLevelRange(Int32.Parse(level.Text)))
+            {
+                newCharacter.characterName = name.Text;
+                newCharacter.level = Int32.Parse(level.Text);
+                newCharacter.determineMaxMP();
+                newCharacter.mp = newCharacter.maxMP;
+
+                Debug.WriteLine("name,level,maxmp " + newCharacter.characterName + " " + newCharacter.level + " " + newCharacter.mp);
+            }
+            else
+            {
+                //Debug.WriteLine("String is empty");
+                MessageBox.Show("Name was left empty or Character level is not 1-20", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+        }
+
+        private void BackToMain(object sender, RoutedEventArgs e)
+        {
+            //exiting character creation without making a new character
+            this.Close();
+        }
+
+        private bool checkLevelRange(int givenLevel)
+        {
+            if (givenLevel >= 1 && givenLevel <= 20)
+                return true;
+
+            return false;
+        }
+    }
+}
